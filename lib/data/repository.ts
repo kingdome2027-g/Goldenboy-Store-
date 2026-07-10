62e3b8a834f2e8c28f268b8b738ebb5fca6d1c1c
kingdome2027-g <kingdome208@gmail.com>
2026-07-10 02:53:02 +0200

    Delete lib directory

--- lib/data/repository.ts
+++
@@ -1,9 +0,0 @@
-import type { CategoryItem, OrderPayload, OrderRecord, ProductItem } from './types';
-
-export interface DataStore {
-  getActiveProducts(): Promise<ProductItem[]>;
-  getProductById(productId: string): Promise<ProductItem | null>;
-  getCategories(): Promise<CategoryItem[]>;
-  createOrder(payload: OrderPayload): Promise<OrderRecord>;
-  findOrderById(orderId: string): Promise<OrderRecord | null>;
-}
